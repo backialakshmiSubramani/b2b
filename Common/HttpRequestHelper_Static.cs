8f40e3780dc7e2a2f70976ec0669cbba247e7a09
Uppunda, Maithree - Dell Team <AMERICAS\Maithree_Uppunda>
2015-05-25 00:02:33 -0500

    Asn Ux  View Shipping Info : Code Checkin

---
+++ Common/HttpRequestHelper_Static.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Modules.Channel.B2B.Common
+{
+    public partial class HttpRequestHelper
+    {
+        /// <summary>
+        /// Gets instance of <see cref="HttpRequestHelper"/> that takes base URL
+        /// </summary>
+        /// <returns></returns>
+        public static HttpRequestHelper Default()
+        {
+            return HttpRequestHelper.FromUrl(ConfigurationReader.GetValue("ASN_BaseURL"));
+        }
+
+        public static HttpRequestHelper FromUrl(string url)
+        {
+            return new HttpRequestHelper(url);
+        }
+
+        //public enum RequestMethod
+        //{
+        //    GET = 0,
+        //    POST
+        //}
+    }
+}
