4211386a2b03f1d34103dea75e29c81120f34815
Saleem, Shaukat - Dell Team <AMERICAS\Shaukat_Saleem>
2017-04-04 01:24:41 -0500

    Automation of Test cases #1543101,#1543548,#1551669,#1551390 ***NO_CI***

--- Core/NewPages/AutoCatalogAndInventoryListPage.cs
+++ Core/NewPages/AutoCatalogAndInventoryListPage.cs
@@ -18,6 +18,7 @@ using Modules.Channel.B2B.Common;
 using Modules.Channel.B2B.Core.Pages;
 using OpenQA.Selenium;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace Modules.Channel.B2B.Core.NewPages
     /// </summary>
     public class AutoCatalogAndInventoryListPage : PageBase
     {
-        IWebDriver webDriver;
+        private readonly IWebDriver _webDriver;
 
         /// <summary>
         /// Constructor to hand off webDriver
@@ -38,51 +39,24 @@ namespace Modules.Channel.B2B.Core.NewPages
         /// <param name="webDriver"></param>
         public AutoCatalogAndInventoryListPage(IWebDriver webDriver) : base(ref webDriver)
         {
-            this.webDriver = webDriver;
+            _webDriver = webDriver;
         }
 
-        private IWebElement SelectCustomerNameSpan
-        {
-            get
-            {
-                return webDriver.FindElement(By.XPath("//span[contains(text(),'Select Customer Profile')]"));
-            }
-        }
+        private IWebElement SelectCustomerNameSpan => _webDriver.FindElement(By.XPath("//span[contains(text(),'Select Customer Profile')]"));
 
-        private IWebElement SelectIdentityNameSpan
-        {
-            get
-            {
-                return webDriver.FindElement(By.XPath("//span[contains(text(),'Select Profile Identity')]"));
-            }
-        }
+        private IWebElement SelectIdentityNameSpan => _webDriver.FindElement(By.XPath("//span[contains(text(),'Select Profile Identity')]"));
 
-        private IWebElement SelectRegionSpan
-        {
-            get
-            {
-                return webDriver.FindElement(By.XPath("//span[contains(text(),'Select Region')]"));
-            }
-        }
+        private IWebElement SelectRegionSpan => _webDriver.FindElement(By.XPath("//span[contains(text(),'Select Region')]"));
 
         /// <summary>
         /// Delta catalog check box
         /// </summary>
-        private IWebElement DeltaCatalogCheckbox
-        {
-            get { return webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Delta")); }
-        }
+        private IWebElement DeltaCatalogCheckbox => _webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Delta"));
 
         /// <summary>
         /// Original Catalog check box
         /// </summary>
-        private IWebElement OriginalCatalogCheckbox
-        {
-            get
-            {
-                return webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Original"));
-            }
-        }
+        private IWebElement OriginalCatalogCheckbox => _webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Original"));
 
         /// <summary>
         /// Selects the status from the Select drop down
@@ -90,46 +64,36 @@ namespace Modules.Channel.B2B.Core.NewPages
         /// <param name="status"></param>
         private void SelectCatalogStatus(string status)
         {
-            webDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(60));
-            webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/a")).Click();
-            webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/div/div[@class='custom-select-search']/input")).SendKeys(status);
-            webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/div/ul/li/a[contains(text(),'" + status + "')]")).Click();
+            _webDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(60));
+            _webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/a")).Click();
+            _webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/div/div[@class='custom-select-search']/input")).SendKeys(status);
+            _webDriver.FindElement(By.XPath("//div[@ng-model='CatalogStatusId']/div/ul/li/a[contains(text(),'" + status + "')]")).Click();
         }
 
-        private IWebElement CatalogsTable
-        {
-            get
-            {
-                return webDriver.FindElement(By.XPath("//table[@st-table='Channel' and @st-safe-src='Catalogs']"));
-            }
-        }
+        private IWebElement CatalogsTable => _webDriver.FindElement(By.XPath("//table[@st-table='Channel' and @st-safe-src='Catalogs']"));
 
-        private IWebElement ClearAllLink
-        {
-            get
-            {
-                return webDriver.FindElement(By.Id("lnkClear"));
-            }
-        }
+        private IEnumerable<IWebElement> CatalogListTableRows => _webDriver.FindElements(By.XPath("//table[@st-safe-src='Catalogs']/tbody/tr"));
+
+        private IWebElement PagingSpan => CatalogsTable.FindElement(By.XPath("thead/tr[1]/td"));
+
+        /// <summary>
+        /// Text box in the paging section
+        /// </summary>
+        private IWebElement PageNumberTextbox => _webDriver.FindElement(By.XPath("//page-select/input"));
+
+        private IWebElement NextButton => _webDriver.FindElement(By.Id("nextUpButton"));
+
+        private IWebElement ClearAllLink => _webDriver.FindElement(By.Id("lnkClear"));
 
         /// <summary>
         /// Catalog Type - Inventory Checkbox
         /// </summary>
-        private IWebElement InventoryCheckbox
-        {
-            get
-            {
-                return webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Inventory"));
-            }
-        }
+        private IWebElement InventoryCheckbox => _webDriver.FindElement(AdeptBy.Attribute(ElementTag.input, "value", "Inventory"));
 
         /// <summary>
         /// Search Records hyperlink
         /// </summary>
-        private IWebElement SearchRecordsLink
-        {
-            get { return webDriver.FindElement(By.Id("lnkSearch")); }
-        }
+        private IWebElement SearchRecordsLink => _webDriver.FindElement(By.Id("lnkSearch"));
 
         private IWebElement GetDownloadButton(int rowIndex)
         {
@@ -139,13 +103,7 @@ namespace Modules.Channel.B2B.Core.NewPages
         /// <summary>
         /// Automated Catalog List Page
         /// </summary>
-        public IWebElement AutoCatalogInventoryListPageLink
-        {
-            get
-            {
-                return webDriver.FindElement(By.XPath("//a[text()=' Auto Catalog & Inventory List']"));
-            }
-        }
+        private IWebElement AutoCatalogInventoryListPageLink => _webDriver.FindElement(By.XPath("//a[text()=' Auto Catalog & Inventory List']"));
 
         /// <summary>
         /// Treat this like a BVT of the page. If Validate does not pass, throw exception and console.writeline a return message into Test Class
@@ -167,24 +125,24 @@ namespace Modules.Channel.B2B.Core.NewPages
 
         public void GoToAutoCatalogAndInventoryListPage(B2BEnvironment b2BEnvironment)
         {
-            webDriver.Navigate().GoToUrl(ConfigurationReader.GetValue("AutoCatalogListPageUrl") + ((b2BEnvironment == B2BEnvironment.Production) ? "P" : "U"));
+            _webDriver.Navigate().GoToUrl(ConfigurationReader.GetValue("AutoCatalogListPageUrl") + ((b2BEnvironment == B2BEnvironment.Production) ? "P" : "U"));
         }
 
         public void GoToAutoCatalogAndInventoryPage()
         {
             AutoCatalogInventoryListPageLink.Click();
-            PageUtility.WaitForPageRefresh(webDriver);
-            webDriver.SwitchTo().Window(WebDriver.WindowHandles.Last());
+            PageUtility.WaitForPageRefresh(_webDriver);
+            _webDriver.SwitchTo().Window(WebDriver.WindowHandles.Last());
         }
 
         public void SwitchToPreviousTab()
         {
-            webDriver.SwitchTo().Window(webDriver.WindowHandles.First());
+            _webDriver.SwitchTo().Window(_webDriver.WindowHandles.First());
         }
 
         public void SwitchToNextTab()
         {
-            webDriver.SwitchTo().Window(WebDriver.WindowHandles.Last());
+            _webDriver.SwitchTo().Window(WebDriver.WindowHandles.Last());
         }
 
         public void SearchCatalog(string profileName, string identityName, DateTime anyTimeAfter, CatalogStatus catalogStatus, CatalogType catalogType)
@@ -194,7 +152,7 @@ namespace Modules.Channel.B2B.Core.NewPages
             SelectOptionFromDropDown(SelectIdentityNameSpan, identityName.ToUpper());
 
             SelectOriginalOrDeltaCheckBox(catalogType);
-            SelectCatalogStatus(UtilityMethods.ConvertToString(catalogStatus));
+            SelectCatalogStatus(catalogStatus.ConvertToString());
             SearchRecordsLink.Click();
             CatalogsTable.WaitForElementVisible(TimeSpan.FromSeconds(30));
             WaitForCatalogInSearchResult(anyTimeAfter.ConvertToUtcTimeZone(), catalogStatus);
@@ -228,10 +186,10 @@ namespace Modules.Channel.B2B.Core.NewPages
         /// <returns>File name for the downloaded catalog</returns>
         public string DownloadCatalog(string identityName, DateTime anyTimeAfter)
         {
-            UtilityMethods.ClickElement(webDriver, GetDownloadButton(1));
+            UtilityMethods.ClickElement(_webDriver, GetDownloadButton(1));
             string downloadPath = ConfigurationManager.AppSettings["CatalogDownloadPath"];
 
-            webDriver.WaitForDownLoadToComplete(downloadPath, identityName, anyTimeAfter, TimeSpan.FromMinutes(1));
+            _webDriver.WaitForDownLoadToComplete(downloadPath, identityName, anyTimeAfter, TimeSpan.FromMinutes(1));
             string fileName = new DirectoryInfo(downloadPath).GetFiles().AsEnumerable()
                 .Where(file => file.Name.Contains(identityName.ToUpper()) && file.CreationTime > anyTimeAfter)
                 .FirstOrDefault().FullName;
@@ -245,64 +203,114 @@ namespace Modules.Channel.B2B.Core.NewPages
             SelectOptionFromDropDown(SelectRegionSpan, "US");
             SelectOptionFromDropDown(SelectCustomerNameSpan, profileName);
             SelectOptionFromDropDown(SelectIdentityNameSpan, identityName.ToUpper());
-            SelectCatalogStatus(UtilityMethods.ConvertToString(catalogStatus));
+            SelectCatalogStatus(catalogStatus.ConvertToString());
             InventoryCheckbox.Click();
             SearchRecordsLink.Click();
             CatalogsTable.WaitForElementVisible(TimeSpan.FromSeconds(30));
             WaitForInventoryInSearchResult(anyTimeAfter.ConvertToUtcTimeZone(), catalogStatus);
         }
 
+        /// <summary>
+        /// Use this method to know if the paging is enabled
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPagingEnabled()
+        {
+            try
+            {
+                const string checktext = "Page of";
+                var text = PagingSpan.Text;
+                if (string.IsNullOrEmpty(PagingSpan.Text) || !text.Contains(checktext))
+                {
+                    Console.WriteLine("Paging not enabled");
+                    return false;
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Paging not enabled");
+                return false;
+            }
+
+            return true;
+        }
+
+        public void DisplayInventoryFeedRecords(string profileName, string identityName, CatalogStatus catalogStatus)
+        {
+            ClearAllLink.Click();
+            SelectOptionFromDropDown(SelectRegionSpan, "US");
+            SelectOptionFromDropDown(SelectCustomerNameSpan, profileName);
+            SelectOptionFromDropDown(SelectIdentityNameSpan, identityName.ToUpper());
+            SelectCatalogStatus(catalogStatus.ConvertToString());
+            InventoryCheckbox.Click();
+            SearchRecordsLink.Click();
+            CatalogsTable.WaitForElementVisible(TimeSpan.FromSeconds(30));
+        }
+
+        public List<string> GetInventoryFeeds()
+        {
+            // ToDo: filter out feeds based on refresh interval, for last 24 hrs if it is less than 12hrs, else latest 2 feeds
+            return CatalogListTableRows.Select(row => row.FindElements(By.TagName("td"))[6].Text).ToList();
+        }
+
+        public void GoToNextPage()
+        {
+            NextButton.Click();
+        }
+
+        public string GetPageNumberFromTextbox()
+        {
+            return PageNumberTextbox.GetAttribute("value");
+        }
+
+        public string GetPagingSpan()
+        {
+            return PagingSpan.Text.Split(' ').Last();
+        }
+
         #region Private Methods
 
         private void SelectOptionFromDropDown(IWebElement webElement, string optionText)
         {
-            string xPath = "../following-sibling::div/child::ul/child::li/a[(text()='" + optionText + "')]";
-            UtilityMethods.SelectOptionFromDDL(webDriver, webElement, xPath);
+            var xPath = "../following-sibling::div/child::ul/child::li/a[(text()='" + optionText + "')]";
+            UtilityMethods.SelectOptionFromDDL(_webDriver, webElement, xPath);
         }
 
         private void WaitForCatalogInSearchResult(DateTime createdTime, CatalogStatus catalogStatus)
         {
-            DateTime lastStatusDate;
-            double timeOutInSecs = CatalogTimeOuts.CatalogSearchTimeOut.TotalSeconds;
-            CatalogStatus status = catalogStatus;
+            var timeOutInSecs = CatalogTimeOuts.CatalogSearchTimeOut.TotalSeconds;
+            var status = catalogStatus;
 
             while (timeOutInSecs > 0)
             {
-                lastStatusDate = Convert.ToDateTime(CatalogsTable.GetCellValue(1, "Last Status Date"), System.Globalization.CultureInfo.InvariantCulture);
+                var lastStatusDate = Convert.ToDateTime(CatalogsTable.GetCellValue(1, "Last Status Date"), System.Globalization.CultureInfo.InvariantCulture);
                 if (CatalogsTable.GetCellValue(1, "Status") != null)
-                    status = UtilityMethods.ConvertToEnum<CatalogStatus>(CatalogsTable.GetCellValue(1, "Status"));
+                    status = CatalogsTable.GetCellValue(1, "Status").ConvertToEnum<CatalogStatus>();
 
                 if (lastStatusDate.AddMinutes(1) > createdTime && (status == catalogStatus || status == CatalogStatus.Failed || status == CatalogStatus.FailedInstant))
                     break;
-                else
-                {
-                    SearchRecordsLink.Click();
-                    Thread.Sleep(TimeSpan.FromSeconds(5));
-                    timeOutInSecs -= 5;
-                }
+                SearchRecordsLink.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                timeOutInSecs -= 5;
             }
         }
 
         private void WaitForInventoryInSearchResult(DateTime createdTime, CatalogStatus catalogStatus)
         {
-            DateTime lastStatusDate;
-            double timeOutInSecs = CatalogTimeOuts.CatalogSearchTimeOut.TotalSeconds;
-            CatalogStatus status = catalogStatus;
+            var timeOutInSecs = CatalogTimeOuts.CatalogSearchTimeOut.TotalSeconds;
+            var status = catalogStatus;
 
             while (timeOutInSecs > 0)
             {
-                lastStatusDate = Convert.ToDateTime(CatalogsTable.GetCellValueForInventory(1, "Last Status Date"), System.Globalization.CultureInfo.InvariantCulture);
+                var lastStatusDate = Convert.ToDateTime(CatalogsTable.GetCellValueForInventory(1, "Last Status Date"), System.Globalization.CultureInfo.InvariantCulture);
                 if (CatalogsTable.GetCellValueForInventory(1, "Status") != null)
-                    status = UtilityMethods.ConvertToEnum<CatalogStatus>(CatalogsTable.GetCellValueForInventory(1, "Status"));
+                    status = CatalogsTable.GetCellValueForInventory(1, "Status").ConvertToEnum<CatalogStatus>();
 
                 if (lastStatusDate.AddMinutes(1) > createdTime && (status == catalogStatus || status == CatalogStatus.Failed || status == CatalogStatus.FailedInstant))
                     break;
-                else
-                {
-                    SearchRecordsLink.Click();
-                    Thread.Sleep(TimeSpan.FromSeconds(5));
-                    timeOutInSecs -= 5;
-                }
+                SearchRecordsLink.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                timeOutInSecs -= 5;
             }
         }
 
