976a308e8c61cfc014206b5bf756118c4ad568d3
Kongara, Rajababu <AMERICAS\Rajababu_Kongara>
2017-04-20 12:54:06 -0500

    Updated the catalog status for CI test cases ***NO_CI***

--- Core/Workflows/Catalog/ChannelUxWorkflow.cs
+++ Core/Workflows/Catalog/ChannelUxWorkflow.cs
@@ -1333,7 +1333,7 @@ namespace Modules.Channel.B2B.Core.Workflows.Catalog
             CPTAutoCatalogInventoryListPage autoCatalogListPage = new CPTAutoCatalogInventoryListPage(webDriver);
             autoCatalogListPage.CatalogsTable.GetCellValue(1, "Last Status Date").Trim().ConvertToDateTime().AddMinutes(1).Should().BeAfter(anyTimeAfter.ConvertToUtcTimeZone(), "Catalog is not displayed in Search Result");
             autoCatalogListPage.CatalogsTable.GetCellValue(1, "Type").Should().Be(catalogType.ConvertToString(), "Expected Catalog type is incorrect");
-            autoCatalogListPage.CatalogsTable.GetCellValue(1, "Status").Should().Be(catalogStatus.ConvertToString(), "Catalog status is not as expected");
+            autoCatalogListPage.CatalogsTable.GetCellValue(1, "Status").Should().Contain(catalogStatus.ConvertToString(), "Catalog status is not as expected");
         }
 
         public void ValidateCatalogSearchResult(CatalogType catalogType, CatalogStatus catalogStatus, DateTime anyTimeAfter)
